21db42785485a47024182476ce7975be5f3666ee
Bhavyagit-hub <102028079+Bhavyagit-hub@users.noreply.github.com>
2022-03-22 10:58:20 -0700

    file is added

---
+++ bhavya.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class bhavya
+{
+	public static void main(String[] args) {
+	   Scanner a = new Scanner(System.in);
+	   char value = a.next().charAt(0);
+	   if(Character.isUpperCase(value)){
+	   System.out.println("Character is in Uppercase");
+	   }
+	   else
+	   {
+	   System.out.println("Character is in Lowercase");
+	}
+}
+}
+
+
